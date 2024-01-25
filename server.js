726177b8c08f518cf2625e2ec2ef7c7b7d7426e8
athul <athulpranoor@gmail.com>
2024-01-25 12:40:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const app = express();
+app.use(express.json());
+app.use(cors());
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "crud",
+});
+
+app.get("/", (req, res) => {
+    const sql = "SELECT * from student";
+    db.query(sql, (err, data) => {
+        if (err) return res.json("Error");
+        return res.json(data);
+    });
+});
+app.post("/create", (req, res) => {
+    const sql = "INSERT INTO student (`Name`, `Email`) VALUES (?)";
+    const values = [req.body.Name, req.body.Email];
+    db.query(sql, [values], (err, data) => {
+        if (values.includes(null)) {
+            return res.json("Error: Name and Email cannot be null");
+        }
+        // if (err) {
+        //     console.error("Error executing SQL query:", err);
+        //     return res.json("Error");
+        // }
+        console.log("Database response:", data);
+        return res.json(data);
+    });
+});
+
+app.put("/update/:id", (req, res) => {
+    const sql = "update student set `Name`= ?, `Email`= ? where ID = ?";
+    const values = [req.body.Name, req.body.Email];
+    const id = req.params.id;
+    db.query(sql, [...values, id], (err, data) => {
+        if (values.includes(null)) {
+            return res.json("Error: Name and Email cannot be null");
+        }
+        // if (err) {
+        //     console.error("Error executing SQL query:", err);
+        //     return res.json("Error");
+        // }
+        console.log("Database response:", data);
+        return res.json(data);
+    });
+});
+app.delete("/delete/:id", (req, res) => {
+    const sql = "DELETE FROM student WHERE ID = ?";
+    const id = req.params.id;
+    db.query(sql, [id], (err, data) => {
+        if (err) return res.json("Error");
+        return res.json(data);
+    });
+});
+
+app.listen(8081, () => {
+    console.log("listening");
+});
