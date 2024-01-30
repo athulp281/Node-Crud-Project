49dfa930916e8e7829a5f183acfe5ea94ec7c343
athul <athulpranoor@gmail.com>
2024-01-30 19:10:33 +0530

    update

--- db.js
+++ db.js
@@ -1,15 +1,10 @@
-const mysql = require("mysql2/promise");
-const { Sequelize, DataTypes } = require("sequelize");
+const mysql = require("mysql");
 
-// const sequelize = new Sequelize("employee_db", "root", "root", {
-//     host: "localhost",
-//     dialect: "mysql", // Use the appropriate dialect for your database
-// });
-const mysqlPool = mysql.createPool({
+const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "crud",
 });
 
-module.exports = mysqlPool;
+module.exports = db;
